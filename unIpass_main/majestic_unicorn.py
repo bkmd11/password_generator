e06cde37fa3cb31536951cfbbcc0c6cfab1c0b01
bkmd11 <bkendall11@gmail.com>
2019-06-22 09:28:09 -0400

    Made this the greatest thing ever with color

--- unIpass_main/majestic_unicorn.py
+++ unIpass_main/majestic_unicorn.py
@@ -1,18 +1,24 @@
+from colorama import init, Fore, Style
+
+
 # Obviously this is just a majestic unicorn
 def majestic_unicorn():
-    print('''
+    init()
+    print(Fore.LIGHTRED_EX + '''
                   ^
                   ^^
-                  ^^^
+                  ^^^ ''' + Fore.LIGHTBLUE_EX + '''
                   ^^^^
                   ^^^^^ $
-                UNIPASSUN$$
+                UNIPASSUN$$''' + Fore.LIGHTGREEN_EX + '''
             UNIPASSUNIPAS$$$
         UNIPASSUN(  )IPASS$$$$
-    UNIPASSUNIPASSUNIPASS$$$$$
+    UNIPASSUNIPASSUNIPASS$$$$$''' + Fore.LIGHTMAGENTA_EX + '''
     UNIPASSUNIPASSUNIPASS$$$$$$
             UNIPASSUNIPAS$$$$$
-            UNIPASSUNIPAS$$$$$$
+            UNIPASSUNIPAS$$$$$$''' + Fore.LIGHTYELLOW_EX + '''
            UNIPASSUNIPASS$$$$$$$$
           UNIPASSUNIPASSU$$$$$$
-''')
+         UNIPASSUNIPASSUN$$$$$$$ 
+''' + Style.RESET_ALL)
+
